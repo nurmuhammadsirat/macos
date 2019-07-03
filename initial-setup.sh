6bd8c36a1718bdb51fee319445952c08c3c2e847
Nur Muhammad Bin Sirat <mat.ace@gmail.com>
2019-07-03 11:50:12 +0800

    Remove automated install for apps that require user intervention.

--- initial-setup.sh
+++ initial-setup.sh
@@ -12,6 +12,9 @@ echo "Installing the following software using brew:"
 echo "  git, docker, autojump"
 brew install git docker autojump
 
+echo "NOTE: to activate autojump, include the following in ~/.zshrc:"
+echo "  [ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh"
+
 echo "Installing the following software using brew cask:"
 echo "  iterm2, sublime-text, intellij-idea, firefox,"
 echo "  alfred, jumpcut, postman, keepassx,"
@@ -19,21 +22,23 @@ echo "  google-backup-and-sync, private-internet-access, vlc,"
 echo "  slack, telegram, whatsapp, virtualbox, virtualbox-extension-pack"
 brew cask install iterm2 sublime-text intellij-idea \
   firefox alfred jumpcut postman keepassx google-backup-and-sync \
-  private-internet-access vlc slack telegram whatsapp \
-  virtualbox virtualbox-extension-pack
+  private-internet-access vlc slack telegram whatsapp
+
+echo "The following software may require user input to allow installation. Please install them manually."
+echo "- brew cask install virtualbox virtualbox-extension-pack"
 
 echo "The following software does not have casks. Manually install them:"
 echo "- BetterTouchTool: https://folivora.ai/"
 
-echo "Installing rbenv..."
-brew install rbenv
-
-echo "Installing ruby-build..."
-brew install ruby-build
+echo "Installing rbenv and ruby-build..."
+brew install rbenv ruby-build
 
 echo "Installing ZSH..."
 brew install zsh zsh-completions
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-echo "  NOTE: Add the plugin zsh-autosuggestions to the list of plugins in .zshrc"
+echo "NOTE: to activate zsh-completions, add the plugin in ~/.zshrc:"
+echo "  plugins=(zsh-autosuggestions)"
+
 chsh -s /bin/zsh
